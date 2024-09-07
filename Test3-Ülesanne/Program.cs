a33b447632db2f843607f9b8d26403f6e981ad27
Andre K. <Andre.kukk@tktk.ee>
2024-09-07 22:00:04 +0300

    add test3 code

---
+++ Test3-Ülesanne/Program.cs
@@ -0,0 +1,38 @@
+﻿char letter;
+Console.WriteLine("Mis hinde sa said?");
+letter = Convert.ToChar(Console.ReadLine()!);
+
+
+if (letter == 'A')
+{
+    Console.WriteLine("Suurepärane!");
+}
+
+ else if (letter == 'B') 
+{   
+    Console.WriteLine("Väga hea!");
+
+}
+
+   else if (letter == 'C')
+{
+    Console.WriteLine("Hea!");
+}
+
+   else if (letter == 'D')
+{
+    Console.WriteLine("Rahuldav!");
+}
+
+   else if (letter == 'E')
+{
+    Console.WriteLine("Kasin!");
+}
+   else if (letter == 'F')
+{ 
+    Console.WriteLine("Puudulik!");
+}
+   else
+{
+    Console.WriteLine("Vale väärtus");
+}
---
+++ Test3-ÜlesanneSwitch/Program.cs
@@ -0,0 +1,28 @@
+﻿char letter;
+Console.WriteLine("Mis hinde sa said?");
+letter = Convert.ToChar(Console.ReadLine()!);
+
+switch(letter)
+{
+    case 'A':
+        Console.WriteLine("Suurepärane");
+            break;
+    case 'B':
+        Console.WriteLine("Väga hea");
+        break;
+    case 'C':
+        Console.WriteLine("Hea");
+        break;
+    case 'D':
+        Console.WriteLine("Rahuldav");
+        break;
+    case 'E':
+        Console.WriteLine("Kasin");
+        break;
+    case 'F':
+        Console.WriteLine("Puudulik");
+        break;
+    default:
+        Console.WriteLine("Vale väärtus");
+        break;
+}   
